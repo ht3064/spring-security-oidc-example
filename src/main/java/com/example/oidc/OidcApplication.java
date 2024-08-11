a2dad44cd3df63ecf1c73b6af4d2244fb8b58a28
현태 최 <ht3064@naver.com>
2024-08-12 03:53:04 +0900

    feat: BaseTimeEntity 생성

--- src/main/java/com/example/oidc/OidcApplication.java
+++ src/main/java/com/example/oidc/OidcApplication.java
@@ -2,8 +2,10 @@ package com.example.oidc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class OidcApplication {
 
 	public static void main(String[] args) {
